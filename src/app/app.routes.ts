a9c3e2e9f0cf4cbff18233829eaaa7f41a82bdbc
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-06-26 18:00:00 +0200

    Update: + Routing (non funzionante)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home-component/home-component';
+import { AboutComponent } from './about-component/about-component';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class MyAppRoutingModule
+{
+
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,21 @@ import { HelloWorldComponent } from './hello-world-component/hello-world-compone
 import { BindingComponent } from './binding-component/binding-component';
 import { DirettiveComponent } from './direttive-component/direttive-component';
 import { PadreComponent } from './padre-component/padre-component';
+import { MyAppRoutingModule } from './app.routes';
+import { HomeComponent } from './home-component/home-component';
+import { AboutComponent } from './about-component/about-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MyComponent, HelloWorldComponent, BindingComponent, DirettiveComponent, PadreComponent],
+  imports: [RouterOutlet, 
+            MyComponent, 
+            HelloWorldComponent, 
+            BindingComponent, 
+            DirettiveComponent, 
+            PadreComponent,
+            MyAppRoutingModule,
+            HomeComponent,
+            AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
