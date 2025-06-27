29edca66007f43aad480850d3a9cd7f1ce1bb0ca
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-06-27 08:49:32 +0200

    Update: + import RouterModule in app.ts (per utilizzare routerLink)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MyComponent } from './my-component/my-component';
 import { HelloWorldComponent } from './hello-world-component/hello-world-component';
 import { BindingComponent } from './binding-component/binding-component';
 import { DirettiveComponent } from './direttive-component/direttive-component';
 import { PadreComponent } from './padre-component/padre-component';
+import { HomeComponent } from './home-component/home-component';
+import { AboutComponent } from './about-component/about-component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
-    MyComponent, 
-    HelloWorldComponent, 
-    BindingComponent, 
-    DirettiveComponent, 
-    PadreComponent],
+            RouterModule,
+            MyComponent, 
+            HelloWorldComponent, 
+            BindingComponent, 
+            DirettiveComponent, 
+            PadreComponent,
+            HomeComponent,
+            AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
